af5d1cf9d8ef31d00833b2ba51164aedfbbc9891
Wilson Soeparman <wilson.soeparman@gmail.com>
2025-06-12 08:52:38 +0800

    feat: Add JWT access token creation functionality and update requirements for security enhancements

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from jose import jwt
+
 from passlib.context import CryptContext
 
 # Membuat context untuk hashing, menentukan bcrypt sebagai algoritma default
@@ -12,3 +16,19 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def hash_password(password: str) -> str:
     """Menghasilkan hash dari password teks biasa."""
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """Membuat JWT access token."""
+    SECRET_KEY = "KUNCI_RAHASIA_ANDA_YANG_SANGAT_SULIT_DITEBAK"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode({None: None}, None, algorithm=ALGORITHM)
+    return encoded_jwt
