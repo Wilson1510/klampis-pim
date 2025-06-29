abbf9343bd0ac74b8eafd629a86165e57c5955ec
Wilson Soeparman <wilson.soeparman@gmail.com>
2025-06-29 12:15:52 +0800

    feat: Introduce Categories model with hierarchical structure and validation constraints

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.core.base import Base
 from app.models.user import Users
 from app.models.category_type import CategoryTypes
+from app.models.category import Categories
 
-__all__ = ["Users", "CategoryTypes", "Base"]
+__all__ = ["Users", "CategoryTypes", "Categories", "Base"]
