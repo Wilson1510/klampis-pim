a6412cebba1f909f4f3723a5c03eaa359b12e43f
Wilson Soeparman <wilson.soeparman@gmail.com>
2025-07-15 23:37:14 +0800

    refactor: update Images model to use AsyncSession for asynchronous parent retrieval and enforce non-nullable is_primary field; enhance tests for field properties and initialization

--- app/models/image_model.py
+++ app/models/image_model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Boolean, Integer
-from sqlalchemy.orm import Session, validates
-from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.asyncio import AsyncSession
 import re
 
 from app.core.base import Base
@@ -12,42 +12,19 @@ class Images(Base):
     """
     file = Column(String(255), nullable=False, unique=True)
     title = Column(String(100), nullable=True)
-    is_primary = Column(Boolean, default=False)
+    is_primary = Column(Boolean, nullable=False, default=False)
 
     object_id = Column(Integer, nullable=False, index=True)
     content_type = Column(String(50), nullable=False, index=True)
 
-    @hybrid_property
-    def parent(self):
+    async def get_parent(self, session: AsyncSession):
         """
-        Dynamic property that automatically finds the parent model and retrieves the
-        object.
-
-        Dynamic Work:
-        1. Get 'registry' from Base SQLAlchemy, which contains all registered models.
-        2. Search for a model whose table name (__tablename__) matches the value of
-        `self.content_type`.
-        3. Once the model is found, it uses session.get() to retrieve the specific
-        object.
+        Retrieve the parent object asynchronously.
         """
-        session = Session.object_session(self)
-        if not session:
-            return None
-
-        # Get 'registry' from Base class model this.
-        # 'self.registry' refers to the registry where the Image model is defined.
-        # 'self.registry.mappers' contains all information about the registered models.
         for mapper in self.registry.mappers:
-            # Get the model class from the mapper
             cls = mapper.class_
-
-            # Check if the model class has __tablename__ and matches the search type
             if hasattr(cls, '__tablename__') and cls.__tablename__ == self.content_type:
-                # The correct model has been found!
-                # Retrieve the object using session.get()
-                return session.get(cls, self.object_id)
-
-        # If the loop completes and no matching model is found, return None.
+                return await session.get(cls, self.object_id)
         return None
 
     @validates('file')
--- tests/models/test_image_model.py
+++ tests/models/test_image_model.py
@@ -1,6 +1,9 @@
-from sqlalchemy import String, event, Text, Integer, CheckConstraint, select
+from sqlalchemy import (
+    String, event, Text, Integer, CheckConstraint, select, Boolean, ForeignKey
+)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.hybrid import hybrid_property
 import pytest
 
 from app.core.base import Base
@@ -20,26 +23,115 @@ from tests.utils.model_test_utils import (
 
 
 @pytest.fixture
-async def setup_images(image_factory):
+async def setup_images(image_factory, category_factory, product_factory):
     """
     Fixture to create images ONCE for the entire test module.
     This is the efficient part.
     """
+    category = await category_factory()
+    product = await product_factory()
     image1 = await image_factory(
-        file="test_folder/test1.jpg"
+        file="test_folder/test1.jpg",
+        object_id=category.id,
+        content_type="categories"
     )
     image2 = await image_factory(
         file="test_folder/test2.jpg",
         title="Test Image 2",
-        is_primary=True
+        is_primary=True,
+        object_id=product.id,
+        content_type="products"
     )
-    return image1, image2
+    return category, product, image1, image2
 
 
 class TestImage:
     @pytest.fixture(autouse=True)
     def setup_objects(self, setup_images):
-        self.test_image1, self.test_image2 = setup_images
+        self.test_category, self.test_product, self.test_image1, \
+            self.test_image2 = setup_images
 
     def test_inheritance_from_base_model(self):
         assert issubclass(Images, Base)
+
+    def test_fields_with_validation(self):
+        assert hasattr(Images, 'validate_file')
+        assert 'file' in Images.__mapper__.validators
+        assert len(Images.__mapper__.validators) == 1
+
+    def test_has_hybrid_property(self):
+        assert isinstance(
+            Images.__dict__['parent'],
+            hybrid_property
+        )
+
+    def test_file_field_properties(self):
+        file_column = Images.__table__.columns.get('file')
+        assert file_column is not None
+        assert isinstance(file_column.type, String)
+        assert file_column.type.length == 255
+        assert file_column.nullable is False
+        assert file_column.unique is True
+        assert file_column.index is None
+        assert file_column.default is None
+
+    def test_title_field_properties(self):
+        title_column = Images.__table__.columns.get('title')
+        assert title_column is not None
+        assert isinstance(title_column.type, String)
+        assert title_column.type.length == 100
+        assert title_column.nullable is True
+        assert title_column.unique is None
+        assert title_column.index is None
+        assert title_column.default is None
+
+    def test_is_primary_field_properties(self):
+        is_primary_column = Images.__table__.columns.get('is_primary')
+        assert is_primary_column is not None
+        assert isinstance(is_primary_column.type, Boolean)
+        assert is_primary_column.nullable is False
+        assert is_primary_column.unique is None
+        assert is_primary_column.index is None
+        assert is_primary_column.default.arg is False
+
+    def test_object_id_field_properties(self):
+        object_id_column = Images.__table__.columns.get('object_id')
+        assert object_id_column is not None
+        assert isinstance(object_id_column.type, Integer)
+        assert object_id_column.nullable is False
+        assert object_id_column.unique is None
+        assert object_id_column.index is True
+        assert object_id_column.default is None
+
+    def test_content_type_field_properties(self):
+        content_type_column = Images.__table__.columns.get('content_type')
+        assert content_type_column is not None
+        assert isinstance(content_type_column.type, String)
+        assert content_type_column.type.length == 50
+        assert content_type_column.nullable is False
+        assert content_type_column.unique is None
+        assert content_type_column.index is True
+        assert content_type_column.default is None
+
+    def test_str_representation(self):
+        assert str(self.test_image1) == "Images(test_folder/test1.jpg)"
+
+    @pytest.mark.asyncio
+    async def test_init_method(self, db_session: AsyncSession):
+        assert self.test_image1.id == 1
+        assert self.test_image1.file == "test_folder/test1.jpg"
+        assert self.test_image1.title is None
+        assert self.test_image1.is_primary is False
+        assert self.test_image1.object_id == 1
+        assert self.test_image1.content_type == 'categories'
+        parent = await self.test_image1.get_parent(db_session)
+        assert parent == self.test_category
+
+        assert self.test_image2.id == 2
+        assert self.test_image2.file == "test_folder/test2.jpg"
+        assert self.test_image2.title == "Test Image 2"
+        assert self.test_image2.is_primary is True
+        assert self.test_image2.object_id == 1
+        assert self.test_image2.content_type == 'products'
+        parent = await self.test_image2.get_parent(db_session)
+        assert parent == self.test_product
